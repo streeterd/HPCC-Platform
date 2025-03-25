7764d32c15779456524b061548ccfde334164a97
Dave Streeter <dave.streeter@lexisnexisrisk.com>
2025-03-25 11:28:34 +0000

    HPCC-27051 Create a Sasha service to clean up post-mortem files
    Debug config to XML instead of YAML.
    
    Signed-off-by: Dave Streeter <dave.streeter@lexisnexisrisk.com>

--- dali/sasha/saserver.cpp
+++ dali/sasha/saserver.cpp
@@ -396,7 +396,7 @@ int main(int argc, const char* argv[])
         else
         {
             PROGLOG("Not connecting to DALISERVERS as no access/dali in config");
-            dbglogYAML(serverConfig);
+            dbglogXML(serverConfig);
         }
         
         if (stop)
